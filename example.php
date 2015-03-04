847f8b5e2165e91c4ef50ac4026237e7191c8ee8
Ivan Dokov <ivangdokov@gmail.com>
2015-03-04 10:26:58 +0200

    Refactored controllers. Renamed them to Calls. Added more examples

--- example.php
+++ example.php
@@ -3,15 +3,36 @@
 require_once 'vendor/autoload.php';
 
 use Eventbrite\Configurations\Configuration;
-use Eventbrite\Controllers\APIController;
+use Eventbrite\Calls\User;
+use Eventbrite\Calls\Event;
 
 $config = require_once 'config.php';
 
 Configuration::baseUri('https://www.eventbriteapi.com');
 Configuration::token($config['oAuth']);
 
-$api = new APIController();
-$events = $api->retrieveUserOwnedEvents($config['userId']);
+/*
+User examples
+*/
+$user = new User();
+
+// $data = $user->getOwnEvents($config['userId']);
+// $data = $user->getOwnDetail();
+// $data = $user->getOwnOrder($config['userId']);
+// $data = $user->getOrderDetails(331687978);
+// $data = $user->getOrderDetails($config['userId']);
+// $data = $user->getOwnEventsAttendees($config['userId']);
+// $data = $user->getVenue($config['userId']);
+
+/*
+Event examples
+*/
+$event = new Event();
+
+// $data = $event->getSearch(['locationAddress' => 'Sofia, Bulgaria', 'locationWithin' => '50km']);
+// $data = $event->getCategoriesList();
+// $data = $event->getDetails('15237119635');
+// $data = $event->getAttendees('15237119635');
 
 echo "<pre>";
-print_r($events);
+print_r($data);
